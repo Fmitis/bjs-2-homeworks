d42531fe652ca104fb8603b05b0b7dd51fc81a8b
Анастасия Лежнева <alzhnev@gmail.com>
2022-06-22 15:06:35 +0500

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 
   return result; // boolean
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  return arr.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
 
   return resultArr; // array
 }
