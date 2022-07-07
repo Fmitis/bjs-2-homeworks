c2736f3260822bf5bb8193ac59d3092811dd5752
Анастасия Лежнева <alzhnev@gmail.com>
2022-07-07 14:55:11 +0500

    second task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -2,62 +2,51 @@
 
 //Задача 1
 
-function parseCount(parseNumber) {
-   let number = Number.parseInt(parseNumber, 10);
-
-   if (isNaN(number)) {
-      throw new Error("Невалидное значение");
+const parseCount = value => {
+   const result = Number.parseInt(value);
+   if (Number.isNaN(result)) {
+      throw new Error('Невалидное значение');
    }
-
-   return number;
+   return result;
 }
-
-function validateCount(unParseNumber) {
+const validateCount = value => {
    try {
-      return parseCount(unParseNumber);
-   } catch (error) {
-      return error;
+      return parseCount(value);
+   } catch (e) {
+      return e;
    }
 }
-
 // Задача 2
 
 class Triangle {
+
    constructor(a, b, c) {
-      if (a + b < c || a + c < b || b + c < a) {
-         throw new Error("Треугольник с такими сторонами не существует!");
-      }
       this.a = a;
       this.b = b;
       this.c = c;
-   }
+
+      if ((a + b) < c || (a + c) < b || (b + c) < a) {
+         throw new Error('Треугольник с такими сторонами не существует');
+      }
+   };
 
    getPerimeter() {
       return this.a + this.b + this.c;
-   }
+   };
 
    getArea() {
-      let halfPerimeter = this.getPerimeter() / 2;
-      return Number(Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3));
-   }
+      let S = this.getPerimeter() / 2;
+      return +Math.sqrt((S * (S - this.a) * (S - this.b) * (S - this.c))).toFixed(3);
+   };
 }
 
-function getTriangle(aSide, bSide, cSide) {
-
-
+function getTriangle(a, b, c) {
    try {
-      return new Triangle(aSide, bSide, cSide);
-   } catch (error) {
-      return new Object({
-         getPerimeter() {
-            return "Ошибка! Такой треугольник не существует";
-         },
-
-         getArea() {
-            return "Ошибка! Такой треугольник не существует";
-         }
-      });
-   }
+      return new Triangle(a, b, c);
+   } catch (e) {
+      return new class SimpleClass {
+         getArea = () => 'Ошибка! Треугольник не существует';
+         getPerimeter = () => 'Ошибка! Треугольник не существует';
+      };
+   };
 }
-
-console.log(getTriangle(1, 10, 3));
